90dc5611a74630189abacf683fbecd9feb56455f
Rafal Gwozdzinski <rafal.gwozdzinski@gmail.com>
2022-12-01 20:28:12 +0100

    Day 01

--- 01.el
+++ 01.el
@@ -1,34 +1,17 @@
-;; Part 1
-(let* ((split-by-calories
-        (mapcar
-         (lambda (x) (split-string x "\n"))
-         (split-string
-          (with-temp-buffer
-            (insert-file-contents-literally "01.txt")
-            (buffer-substring-no-properties (point-min) (point-max))) "\n\n")))
-       (sum-calories (lambda (cal-list)
-                       (apply '+ (mapcar (lambda (x) (string-to-number x)) cal-list))))
-       (sorted (sort
-                (mapcar sum-calories split-by-calories)
+(defvar split-by-calories
+  (mapcar
+   (lambda (x) (split-string x "\n"))
+   (split-string
+    (with-temp-buffer
+      (insert-file-contents-literally "01.txt")
+      (buffer-substring-no-properties (point-min) (point-max))) "\n\n")))
+
+(defun sum-calories (cal-list)
+  (apply '+ (mapcar (lambda (x) (string-to-number x)) cal-list)))
+
+(defvar sorted (sort
+                (mapcar 'sum-calories split-by-calories)
                 '>))
-       )
-  (car sorted))
 
-;; Part 2
-(let* ((split-by-calories
-        (mapcar
-         (lambda (x) (split-string x "\n"))
-         (split-string
-          (with-temp-buffer
-            (insert-file-contents-literally "01.txt")
-            (buffer-substring-no-properties (point-min) (point-max))) "\n\n")))
-       (sum-calories (lambda (cal-list)
-                       (apply '+ (mapcar (lambda (x) (string-to-number x)) cal-list))))
-       (sorted (sort
-                (mapcar sum-calories split-by-calories)
-                '>))
-       )
-  (apply '+ (seq-take sorted 3)))
-
-
-
+(defvar part-1 (car sorted))
+(defvar part-2 (apply '+ (seq-take sorted 3)))
